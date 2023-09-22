3d94f3028ddc9b322cb7c74b6737c4fdae90bcad
sohamingle <sohamingle@gmail.com>
2023-09-22 20:04:09 +0530

    order

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { OrderColumn, columns } from "./column";
+import { DataTable } from "@/components/ui/data-table";
+
+interface Props{
+    data:OrderColumn[]
+}
+
+const OrderClient:React.FC<Props> = ({data}) => {
+
+    const router = useRouter()
+    const params = useParams()
+
+    const title = `Products (${data.length})`
+
+    return (
+        <>
+            <Heading title={title}
+            description="Manage billboards for your store"/>
+            <Separator/>
+            <DataTable columns={columns} data={data} searchKey="products"/>
+        </>
+    );
+}
+
+export default OrderClient;
